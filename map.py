81287aaa8f61a45faf7675249314e605cb991d66
thimarcal <thiago.gm.pereira@gmail.com>
2018-10-07 00:05:30 -0300

    Só falta validar as questões de dados errados

--- map.py
+++ map.py
@@ -1,11 +1,32 @@
 # coding: utf-8
+import os
+import re
+import sys
 
-import sys, re
+# Indexes are Python already (0-start)
+STATION_ID_POS = 0
+UTC_DATE_POS = 1
+UTC_TIME_POS = 2
+LONGITUDE_POS = 6
+LATITUDE_POS = 7
+AVG_TEMP_POS = 9
+MAX_TEMP_POS = 10
+MIN_TEMP_POS = 11
 
-for line in sys.stdin:
+#os.chdir('D:\\Thiago\\Studies\\Unicamp\\MDC\\INF-617\\Tarefas\\INF0617_Tarefa2')
+os.chdir('/mnt/d/Thiago/Studies/Unicamp/MDC/INF-617/Tarefas/INF0617_Tarefa2')
 
-	line = re.sub('[^a-z]', ' ', line.lower())
+def getLines(filename):
+    file = open('./weather-2017/' + filename, encoding='utf-8', errors='ignore')
+    for line in file:
+        data = line.split()
+        if not data[MAX_TEMP_POS].startswith('-99') and not data[MIN_TEMP_POS].startswith('-99') and \
+                (float(data[MAX_TEMP_POS]) >= float(data[MIN_TEMP_POS])) and (float(data[MAX_TEMP_POS]) < 60.0):
+            print(data[STATION_ID_POS] + "," + data[LATITUDE_POS] + " " + data[LONGITUDE_POS] + " " + data[UTC_DATE_POS] +
+                  " " + data[UTC_TIME_POS] + " " + str(abs(abs(float(data[MAX_TEMP_POS])) - abs(float(data[MIN_TEMP_POS])))))
 
-	words = line.split()
-	for word in words:
-		print(word + ",1")
+
+# Parse entries
+if __name__ == '__main__':
+    for filename in sys.stdin:
+        getLines(filename.replace('\n', ''))
--- reduce.py
+++ reduce.py
@@ -2,13 +2,41 @@
 
 import sys
 
-words = {}
-for line in sys.stdin:
-	word, count = line.split(",")
-	try:
-		words[word] += int(count)
-	except:
-		words[word] = int(count)
+DIFF = 'difference'
+TIME = 'time'
+DATE = 'date'
+LAT = 'latitude'
+LONG = 'longitude'
 
-for word in words.keys():
-	print(str(word)+", "+str(words[word]))
+def format_date(dateStr):
+    return dateStr[0:4]+"-"+dateStr[4:6]+"-"+dateStr[6:]
+
+stations = {}
+for line in sys.stdin:
+    station, data = line.split(",")
+    longitude, latitude, date, time, difference = data.split()
+
+    try:
+        if float(stations[station][DIFF]) < float(difference):
+            print(longitude, latitude, date, time, difference)
+            stations[station][DATE] = date
+            stations[station][TIME] = time
+        stations[station][DIFF] = max(float(stations[station][DIFF]), float(difference))
+    except:
+        stations[station] = {}
+        print("Adding for "+station, difference)
+        stations[station][DIFF] = difference
+        stations[station][LAT] = latitude
+        stations[station][LONG] = longitude
+        stations[station][DATE] = date
+        stations[station][TIME] = time
+
+max_diff = {}
+max = 0.0
+for station in stations.keys():
+    if float(stations[station][DIFF]) > max:
+        max = float(stations[station][DIFF])
+        max_diff = stations[station]
+
+print("Station at (" + str(max_diff[LAT]) + ", " + str(max_diff[LONG]) + ") measured " + str(max_diff[DIFF])
+      + " degree(s) difference at " + format_date(str(max_diff[DATE])) + " " + str(max_diff[TIME]))
